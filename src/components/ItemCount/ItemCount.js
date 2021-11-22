cc712987ac49bc032c1342bce44c71a11fb10b5e
Kan <kan_a_n@hotmail.com>
2021-11-22 17:11:46 -0300

    Correccion

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,52 +1,16 @@
 import './ItemCount.css'
-import { useState } from 'react'
-import { Link, Route } from 'react-router-dom'
+//import { useState } from 'react'
 
-
-function ItemCount({ stock }) {
-
-    const InputCount = () => {
-
-        return <Link to="/cart"><button onClick={() => <Route exact path="/cart"></Route>} >Ir al Cart o Terminar compra</button></Link>
-    }
-
-    const ButtonCount = ({ handleInter }) => {
-        return <button onClick={handleInter}>Agregar Al carrito</button>
-
-    }
-
-    const [inputType, setInputType] = useState('button')
-
-    const handleInter = () => {
-        setInputType('input')
-    }
-
-    const [count, setCount] = useState(0);
-
-    const onAdd = () => {
-        setCount(count + 1)
-    }
-
-    const onSub = () => {
-        setCount(count - 1)
-    }
+function ItemCount({ stock, ButtonCount, handleInter, count, onAdd, onSub }) {
 
     return (
         <div className='producto'>
-            <p>Camisa</p>
             <div className='contador'>
                 <button onClick={() => count > 0 && onSub()}>-</button>
                 <p>{count}</p>
                 <button onClick={() => count < stock && onAdd()}>+</button>
             </div>
-
-            {
-                inputType === 'button' ?
-                    <ButtonCount handleInter={handleInter} />
-
-                    :
-                    <InputCount />
-            }
+            <ButtonCount handleInter={handleInter} />
         </div>
     )
 }
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,8 +1,23 @@
 import ItemCount from '../ItemCount/ItemCount';
+import { useState } from 'react';
+import { Link, Route } from 'react-router-dom'
 
-function ItemDetail(product) {
+function ItemDetail({ product, count, setCount, onAdd, onSub }) {
     const { title, img, price, description, id, stock } = product
 
+    const [inputType, setInputType] = useState('button')
+
+    const handleInter = () => {
+        setInputType('input')
+    }
+
+    const InputCount = () => {
+        return <Link to="/cart"><button onClick={() => <Route exact path="/cart"></Route>} >Ir al Cart o Terminar compra</button></Link>
+    }
+
+    const ButtonCount = ({ handleInter }) => {
+        return <button onClick={handleInter}>Agregar Al carrito</button>
+    }
 
     return (
         <div key={id} className="producto">
@@ -12,7 +27,14 @@ function ItemDetail(product) {
             <h2>{description}</h2>
             <h3>Stock: {stock}</h3>
 
-            <ItemCount stock={stock} />
+
+            {
+                inputType === 'button' ?
+                    <ItemCount stock={stock} handleInter={handleInter} ButtonCount={ButtonCount} count={count} onAdd={onAdd} onSub={onSub} />
+                    :
+                    < InputCount />
+            }
+
 
         </div>
     )
